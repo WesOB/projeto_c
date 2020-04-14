91c4f34a73157b272ae13a2cbee4ffdb3b3b504a
wesx <wesx.si@gmail.com>
2020-04-14 16:51:45 -0300

    add prog_c04

---
+++ programa-c04/prog_c04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	printf("Default: %f\n", 3.1415169265);
+	printf("Uma casa: %.1f\n", 3.1415169265);
+	printf("Duas casas: %.2f\n", 3.1415169265);
+	printf("Três casas: %.3f\n", 3.1415169265);
+	printf("Quatro casas: %.4f\n", 3.1415169265);
+	printf("Notação Científica: %e\n", 3.1415169265);
+}
