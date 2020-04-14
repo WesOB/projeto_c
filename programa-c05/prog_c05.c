ec5e187223e92a354a67c0fcb4c6caf92c138c8c
wesx <wesx.si@gmail.com>
2020-04-14 17:32:25 -0300

    add prog_c05

---
+++ programa-c05/prog_c05.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	printf("Ex-01\n");
+	printf("Minha cidade natal é: %s\n\n","Berlim");
+	printf("Ex-02\n");
+	printf("nome: %s\n","Jack");
+	printf("idade: %d\n", 29);
+	printf("altura: %.2f\n\n",1.752);
+	printf("Ex-03\n");
+	printf("%.1f\n", 75.7632489);
+	printf("%.2f\n", 75.7632489);
+	printf("%.5f\n", 75.7632489);
+	
+}
