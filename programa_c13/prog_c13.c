aa4e2cf1039bb8c8943890df49f62dbf1a18abc2
Wex <wesley.tadsi@gmail.com>
2020-05-27 22:01:36 -0300

    add prog_c13

---
+++ programa_c13/prog_c13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+int x = 10, y=3;
+
+if(x<10)
+printf("Primeira Saida \n");
+else
+if(y<4)
+if(x>10)
+printf("Segunda Saida\n");
+else
+ printf("Terceira Saida\n");
+return 0;
+
+
+}
